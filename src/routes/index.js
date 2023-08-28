29115b5cc517cbded5bc15a5689d8d90f7802f9c
EdgyHairu <jairarmando2012@outlook.com>
2023-08-28 09:08:00 -0600

    Modificacion de archivos

--- src/routes/index.js
+++ src/routes/index.js
@@ -14,3 +14,41 @@ router.get('/', async(req, res) => {
         console.log('Error ', error)
     }
 })
+
+router.post('/new-contact', async(req, res) => {
+    const { firstname, lastname, email, phone } = req.body
+    await database.collection('contacts').add({
+        firstname,
+        lastname,
+        email,
+        phone
+    })
+    res.redirect('/')
+})
+
+router.get('/delete-contact/:id', async(req, res) => {
+    await db.collection('contacts').doc(req.params.id).delete()
+    res.redirect('/')
+})
+
+router.get('/edit-contact/:id', async(req, res) => {
+    const doc = await db.collection('contacts').doc(req.params.id).get()
+    res.render('index', { contact: {
+        id: doc.id,
+        ...doc.data()
+    }}) 
+})
+
+router.post('/update contact/:id', async (req, res) => {
+    const { firstname, lastname, email, phone} = req.body
+    const id = req.params
+    await db.collection('contacts').doc(id).update({
+        firstname,
+        lastname, 
+        email,
+        phone
+    })
+    res.redirect('/')
+})
+
+module.exports = router
